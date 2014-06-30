eef017f05343a8664120d8faa39a5608f3ddbcfd
mhartington <mikehartington@gmail.com>
2014-06-30 00:54:56 -0400

    update to beta 8 and added alarm functionality

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,34 +1,36 @@
 angular.module('starter.controllers', [])
 
-.controller('WorldClockCtrl', function ($scope) {})
+.controller('WorldClockCtrl', function($scope) {})
 
-.controller('AlarmCtrl', function ($scope, $ionicModal, AlarmService) {
-    $scope.data = {
-        showDelete: false
-    };
-
-
-    $scope.times = AlarmService.all();
-    var newId = 0;
-    $scope.add = function () {
-        newId += 1;
-        $scope.times.push({
-            'alarm': '6:30',
-            'id': newId,
-            'checked': true
-        });
-
-        $scope.modal.hide();
-    };
+.controller('AlarmCtrl', function($scope, $ionicModal, AlarmService) {
+  $scope.data = {
+    showDelete: false
+  };
 
 
-    $ionicModal.fromTemplateUrl('templates/alarm-modal.html', function (modal) {
-        $scope.modal = modal;
-    }, {
-        scope: $scope,
-        animation: 'slide-in-up',
-        focusFirstInput: true
+  $scope.times = AlarmService.all();
+
+  var newId = 0;
+
+  $scope.add = function(alarm) {
+    newId += 1;
+    $scope.times.push({
+      'alarm': alarm.alarm,
+      'id': newId,
+      'checked': true
     });
+
+    $scope.modal.hide();
+  };
+
+
+  $ionicModal.fromTemplateUrl('templates/alarm-modal.html', function(modal) {
+    $scope.modal = modal;
+  }, {
+    scope: $scope,
+    animation: 'slide-in-up',
+    focusFirstInput: true
+  });
 
 
 
@@ -36,25 +38,25 @@ angular.module('starter.controllers', [])
 
 // $scope.modalData.msg = msg;
 
-.controller('ModalCtrl', function ($scope) {
+// .controller('ModalCtrl', function ($scope) {
+//
+//     var newId = 0;
+//     $scope.add = function (msg) {
+//         newId += 1;
+//         $scope.times.push({
+//             'alarm': msg,
+//             'id': newId,
+//             'checked': true
+//         });
+//
+//         $scope.modal.hide();
+//     };
+//
+//
+// })
 
-    var newId = 0;
-    $scope.add = function (msg) {
-        newId += 1;
-        $scope.times.push({
-            'alarm': msg,
-            'id': newId,
-            'checked': true
-        });
-
-        $scope.modal.hide();
-    };
-
+.controller('TimerCtrl', function($scope) {
 
 })
 
-.controller('TimerCtrl', function ($scope) {
-
-})
-
-.controller('StopWatchCtrl', function ($scope) {});
+.controller('StopWatchCtrl', function($scope) {});
